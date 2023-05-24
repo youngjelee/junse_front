d65faff81af7e8fb4da9610a8b1ffd3f15797e01
youngjelee <wpdud1221@naver.com>
2023-05-25 00:50:33 +0900

    메인 layout 컨텐츠 부분 추가

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,17 +1,24 @@
 import Form from 'react-bootstrap/Form';
+import React, { useEffect } from 'react';
 
-function FormText() {
+function FormText(props) {
+  
+  useEffect(() => {
+    console.log(props.children);
+  }, []);
+
+  
   return (
     <>
       {/* <Form.Label htmlFor="inputPassword5">Password</Form.Label> */}
       <Form.Control
-        type="password"
-        id="inputPassword5"
-        aria-describedby="passwordHelpBlock"
+        type="input"
+        // id="inputPassword5"
+        // aria-describedby="passwordHelpBlock"
       />
       <Form.Text id="passwordHelpBlock" muted>
-        Your password must be 8-20 characters long, contain letters and numbers,
-        and must not contain spaces, special characters, or emoji.
+        {/* Your password must be 8-20 characters long, contain letters and numbers,
+        and must not contain spaces, special characters, or emoji. */}
       </Form.Text>
     </>
   );
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,42 @@
 import React from 'react'
 import FormComponent from "../components/FormComponent"
 import ImageComponent from '../components/ImageComponent'
+import styled from "styled-components";
+
+const MainContens = styled.div `
+    display: flex;
+    width: 1080px;
+    margin: 36px auto 0px;
+    box-sizing: border-box; 
+`
+
+const SectionOne = styled.section ` 
+    flex-basis: 536px;
+    margin-right: 8px;
+    outline: 1px solid #ced4da;
+`
+
+const SctionTwo = styled.section`
+    -webkit-box-flex: 1;
+    flex-grow: 1;
+    max-width: 536px;
+    outline: 1px solid #ced4da;
+`
+
+
 function Main() {
   return (
         <>
             <ImageComponent src="https://opgg-static.akamaized.net/logo/20230521155046.f443812c43b2435ea8c7892bbcd6b995.png?image=q_auto,f_webp,w_auto&v=1684731456767" width="300px" height="300px"/>
             <FormComponent/>
+
+            <MainContens>  
+                <SectionOne >섹션 원 </SectionOne>
+
+                <SctionTwo>섹션 투</SctionTwo>
+
+            </MainContens>
+
         </>
 
     )
